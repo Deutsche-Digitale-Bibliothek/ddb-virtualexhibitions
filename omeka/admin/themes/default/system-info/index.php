16515dac7d783b0a4763deb32184f0810ddc8689
Viktor Grandgeorg <info@intelligibel.de>
2017-06-08 20:36:21 +0200

    Add values from php-ini to sysinfo

--- omeka/admin/themes/default/system-info/index.php
+++ omeka/admin/themes/default/system-info/index.php
@@ -13,6 +13,23 @@ echo head(array('title' => $title, 'bodyclass' => 'system-info')); ?>
     </tr>
     <?php endforeach; ?>
 <?php endforeach; ?>
+    <tr><th colspan="2">PHP Kofiguration</th></tr>
+    <tr>
+        <td>upload_max_filesize</td>
+        <td><?php echo ini_get('upload_max_filesize'); ?></td>
+    </tr>
+    <tr>
+        <td>post_max_size</td>
+        <td><?php echo ini_get('post_max_size'); ?></td>
+    </tr>
+    <tr>
+        <td>max_execution_time</td>
+        <td><?php echo ini_get('max_execution_time'); ?></td>
+    </tr>
+    <tr>
+        <td>memory_limit</td>
+        <td><?php echo ini_get('memory_limit'); ?></td>
+    </tr>
 </table>
 <?php fire_plugin_hook('admin_system_info', array('system_info' => $info, 'view' => $this)); ?>
 <?php echo foot();
