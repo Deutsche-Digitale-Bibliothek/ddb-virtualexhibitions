5c24e822a937a57afcd4093e12ed2ffe2a6e91a0
Viktor Grandgeorg <info@intelligibel.de>
2017-07-06 09:50:10 +0200

    Change fieldset heading (h2) to legend.

--- omeka/admin/themes/default/items/form.php
+++ omeka/admin/themes/default/items/form.php
@@ -53,7 +53,7 @@ jQuery(document).bind('omeka:elementformload', function (event) {
         <?php if (!empty($tabContent)): ?>
             <div id="<?php echo text_to_id(html_escape($tabName)); ?>-metadata">
             <fieldset class="set">
-                <h2><?php echo html_escape(__($tabName)); ?></h2>
+                <legend><?php echo html_escape(__($tabName)); ?></legend>
                 <?php echo $tabContent; ?>
             </fieldset>
             </div>
