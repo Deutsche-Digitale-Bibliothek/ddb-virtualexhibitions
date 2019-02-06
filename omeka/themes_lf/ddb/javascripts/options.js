af9027196b8747aef6bd096e417c6205753b1ac3
Viktor Grandgeorg <viktor@grandgeorg.de>
2019-02-06 15:17:39 +0100

    Add section-cite.

--- omeka/themes_lf/ddb/javascripts/options.js
+++ omeka/themes_lf/ddb/javascripts/options.js
@@ -1,4 +1,4 @@
 litfassOptions = {
-  anchors: ['s0', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8'],
-  sectionsColor: ['#e8b4b4', '#3d4f58', '#ccdad6', '#3d4f58', '#f7cbba', '#e3f3fe', '#f6f9ea', '#e8b4b4', '#ccdad6']
+  anchors: ['s0', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9'],
+  sectionsColor: ['#e8b4b4', '#3d4f58', '#ccdad6', '#3d4f58', '#f7cbba', '#e3f3fe', '#f6f9ea', '#e8b4b4', '#ccdad6', '#e3f3fe']
 }
