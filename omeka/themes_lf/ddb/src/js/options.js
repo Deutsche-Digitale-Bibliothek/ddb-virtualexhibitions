dc69ee516670f9b7c67613290ae05081943fdc95
Viktor Grandgeorg <viktor@grandgeorg.de>
2019-02-13 12:55:33 +0100

    Add section-container-pos.

--- omeka/themes_lf/ddb/src/js/options.js
+++ omeka/themes_lf/ddb/src/js/options.js
@@ -211,7 +211,7 @@ var litfassColorPalettes = {
 };
 
 var litfassOptions = {
-  anchors: ['s0', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9'],
+  anchors: ['s0', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10'],
   sectionsColor: [
     litfassColorPalettes.c.rose.hex,
     litfassColorPalettes.c.grey.hex,
@@ -222,6 +222,7 @@ var litfassOptions = {
     litfassColorPalettes.c.sand.hex,
     litfassColorPalettes.c.rose.hex,
     litfassColorPalettes.c.green.hex,
-    litfassColorPalettes.c.blue.hex
+    litfassColorPalettes.c.blue.hex,
+    litfassColorPalettes.c.sand.hex
   ]
 };
