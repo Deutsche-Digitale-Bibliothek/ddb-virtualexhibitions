d8d39b190cba9a8bc61da704cb3713d4656058e0
Viktor Grandgeorg <viktor@grandgeorg.de>
2019-03-21 17:02:20 +0100

    Add blockquote template

---
+++ omeka/plugins_lf/ExhibitBuilder/views/shared/exhibit_layouts/ddb-litfass-quote/form.php
@@ -0,0 +1,8 @@
+<fieldset class="<?php echo html_escape($layout); ?>">
+    <div class="section">
+        <h4><?php echo __('Zitatblock'); ?></h4>
+        <?php echo exhibit_builder_layout_form_text(1); ?>
+        <h4><?php echo __('Quelleangaben'); ?></h4>
+        <?php echo exhibit_builder_layout_form_text(2); ?>
+    </div>
+</fieldset>
