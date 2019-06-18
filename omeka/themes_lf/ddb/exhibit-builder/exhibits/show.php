efe7530de54f46bf26406f20a338aa934426b5ca
Viktor Grandgeorg <viktor@grandgeorg.de>
2019-06-18 21:59:51 +0200

    Add counter and isSlider for sections

--- omeka/themes_lf/ddb/exhibit-builder/exhibits/show.php
+++ omeka/themes_lf/ddb/exhibit-builder/exhibits/show.php
@@ -2,10 +2,22 @@
 $host  = $_SERVER['HTTP_HOST'] . WEB_DIR;
 $pageParent = $exhibit_page->getParent();
 $pageSiblings = ($pageParent ? exhibit_builder_child_pages($pageParent) : $exhibit->getTopPages());
+$counter = 0;
+$inSlider = false;
 foreach ($pageSiblings as $key => $pageSibiling) {
+    $pageoptions = unserialize($pageSibiling->pageoptions);
+    if (isset($pageoptions['slider']) && $pageoptions['slider'] === 'start') {
+      $inSlider = true;
+      $counter++;
+    }
+    if (isset($pageoptions['slider']) && $pageoptions['slider'] === 'end') {
+      $inSlider = false;
+    }
+    if ($inSlider === false && (!isset($pageoptions['slider']) || $pageoptions['slider'] !== 'end')) {
+      $counter++;
+    }
     if ($pageSibiling->id == $exhibit_page->id) {
-        // var_dump('Location: ' . WEB_DIR . '/#s' . ($key + 1));
-        header('Location: ' . WEB_DIR . '/#s' . ($key + 1), TRUE, 301);
+        header('Location: ' . WEB_DIR . '/#s' . $counter, TRUE, 301);
     }
 }
 exit;
