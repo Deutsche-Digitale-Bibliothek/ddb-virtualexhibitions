452687c191d381e07dc4eea984556fc53be96aa3
Viktor Grandgeorg <viktor@grandgeorg.de>
2019-06-19 14:35:59 +0200

    Add cookie notice bar

--- omeka/themes_lf/ddb/src/js/app.js
+++ omeka/themes_lf/ddb/src/js/app.js
@@ -8,6 +8,14 @@
   var zoomHintShown = false;
   var slideHeightOffset = 43;
 
+  // Creare's 'Implied Consent' EU Cookie Law Banner v:2.4
+  // Conceived by Robert Kent, James Bavington & Tom Foyster
+
+  var dropCookie = true;                      // false disables the Cookie, allowing you to style the banner
+  var cookieDuration = 14;                    // Number of days before the cookie expires, and the banner reappears
+  var cookieName = 'complianceCookie';        // Name of our cookie
+  var cookieValue = 'on';                     // Value of cookie
+
   // var pubsub = (function () {
 
   //   var events = {};
@@ -843,6 +851,59 @@
     });
   }
 
+  function createCookieDiv() {
+    var div = $('<div id="cookie-law" class="cookie-law"><p>Um unser Internetangebot für Sie optimal gestalten und fortlaufend verbessern zu können, verwenden wir Cookies. Durch die weitere Nutzung unseres Angebots erklären Sie sich hiermit einverstanden. Wenn Sie der Nutzungsanalyse widersprechen oder mehr über Cookies erfahren möchten, klicken Sie bitte auf die <a href="https://www.deutsche-digitale-bibliothek.de/content/datenschutzerklaerung" rel="noopener" target="_blank">Datenschutzerklärung</a>.</p></div>');
+    var button = $('<button id="close-cookie-notice" type="button" class="close-cookie-notice close btn btn-secondary" aria-label="Close" aria-controls="cookie-notice"><span aria-hidden="true">&times;</span></button>');
+    div.append(button);
+    $('body').prepend(div);
+    button.on('click', function() {
+      dispathCookie();
+    });
+  }
+
+  function createCookie(name, value, days) {
+    var expires = '';
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = '; expires=' + date.toGMTString();
+    }
+    if (dropCookie) {
+      document.cookie = name + '=' + value + expires + '; path=/';
+    }
+  }
+
+  function checkCookie(name) {
+    var nameEQ = name + '=';
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+
+  function eraseCookie(name) {
+    createCookie(name, '', -1);
+  }
+
+  function dispathCookie() {
+    // Create the cookie only if the user click on "Close"
+    createCookie(cookieName,cookieValue, cookieDuration); // Create the cookie
+    // then close the window/
+    var element = document.getElementById('cookie-law');
+    element.parentNode.removeChild(element);
+  }
+
+  function bindCookieNotice() {
+    $(window).on('load', function() {
+      if(checkCookie(cookieName) != cookieValue){
+        createCookieDiv();
+      }
+    });
+  }
+
   function init() {
     $(function() {
       setMenuProps();
@@ -860,6 +921,7 @@
       bindZoom();
       bind3D();
       bindEmptyClick();
+      bindCookieNotice();
     });
   }
 
