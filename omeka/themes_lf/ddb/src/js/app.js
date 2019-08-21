d780b0d4c0b20f985a936ef32a72456782d34534
Viktor Grandgeorg <viktor@grandgeorg.de>
2019-08-21 12:56:39 +0200

    Implement screenfull.js

--- omeka/themes_lf/ddb/src/js/app.js
+++ omeka/themes_lf/ddb/src/js/app.js
@@ -972,32 +972,31 @@
     });
   }
 
-  function openFullscreen(element) {
-    if (typeof element === 'undefined') {
-      element = document.documentElement;
-    }
-    if (element.requestFullscreen) {
-      element.requestFullscreen();
-    } else if (element.mozRequestFullScreen) {
-      element.mozRequestFullScreen();
-    } else if (element.webkitRequestFullscreen) {
-      element.webkitRequestFullscreen();
-    } else if (element.msRequestFullscreen) {
-      element.msRequestFullscreen();
-    }
-  }
+  // function openFullscreen() {
+  //   var element = document.documentElement;
 
-  function closeFullscreen() {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.mozCancelFullScreen) {
-      document.mozCancelFullScreen();
-    } else if (document.webkitExitFullscreen) {
-      document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) {
-      document.msExitFullscreen();
-    }
-  }
+  //   if (element.requestFullscreen) {
+  //     element.requestFullscreen();
+  //   } else if (element.mozRequestFullScreen) {
+  //     element.mozRequestFullScreen();
+  //   } else if (element.webkitRequestFullscreen) {
+  //     element.webkitRequestFullscreen();
+  //   } else if (element.msRequestFullscreen) {
+  //     element.msRequestFullscreen();
+  //   }
+  // }
+
+  // function closeFullscreen() {
+  //   if (document.exitFullscreen) {
+  //     document.exitFullscreen();
+  //   } else if (document.mozCancelFullScreen) {
+  //     document.mozCancelFullScreen();
+  //   } else if (document.webkitExitFullscreen) {
+  //     document.webkitExitFullscreen();
+  //   } else if (document.msExitFullscreen) {
+  //     document.msExitFullscreen();
+  //   }
+  // }
 
   function bindFullscreen() {
     $('#toggle-fullsize').on('click', function(e) {
@@ -1011,17 +1010,23 @@
         activeSlide = 0;
       }
       if (control.hasClass('active')) {
-        closeFullscreen();
-        control.removeClass('active');
-        setTimeout(function() {
-          $.fn.fullpage.moveTo(activeSection.anchor, activeSlide);
-        }, 400);
+        if (screenfull.enabled) {
+          screenfull.request();
+          // closeFullscreen();
+          control.removeClass('active');
+          setTimeout(function() {
+            $.fn.fullpage.moveTo(activeSection.anchor, activeSlide);
+          }, 400);
+        }
       } else {
-        openFullscreen();
-        control.addClass('active');
-        setTimeout(function() {
-          $.fn.fullpage.moveTo(activeSection.anchor, activeSlide);
-        }, 400);
+        if (screenfull.enabled) {
+          screenfull.exit();
+          // openFullscreen();
+          control.addClass('active');
+          setTimeout(function() {
+            $.fn.fullpage.moveTo(activeSection.anchor, activeSlide);
+          }, 400);
+        }
       }
     });
   }
