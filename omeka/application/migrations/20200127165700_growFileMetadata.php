9141a4f0248a042b610d161efdbc68b253e73e23
grandgeorg <viktor@grandgeorg.de>
2020-04-09 13:19:38 +0200

    Set default timestamp value

--- omeka/application/migrations/20200127165700_growFileMetadata.php
+++ omeka/application/migrations/20200127165700_growFileMetadata.php
@@ -15,6 +15,9 @@ class growFileMetadata extends Omeka_Db_Migration_AbstractMigration
 {
     public function up()
     {
+        // Grandgeorg Websolutions:
+        $this->db->query("ALTER TABLE {$this->db->File} CHANGE `added` `added` timestamp NOT NULL DEFAULT '2000-01-01 00:00:00' AFTER `modified`");
+        // :Grandgeorg Websolutions
         $this->db->query("ALTER TABLE {$this->db->File} MODIFY `metadata` MEDIUMTEXT COLLATE utf8_unicode_ci NOT NULL");
     }
 }
