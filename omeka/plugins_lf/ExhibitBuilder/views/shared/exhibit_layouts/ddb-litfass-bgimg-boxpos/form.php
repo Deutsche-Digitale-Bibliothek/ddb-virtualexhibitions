a2dfa4ad0aa06f3467962d5ffa3d3c41284fea3b
grandgeorg <viktor@grandgeorg.de>
2020-06-10 13:51:18 +0200

    Resolve #DDBVA-481

--- omeka/plugins_lf/ExhibitBuilder/views/shared/exhibit_layouts/ddb-litfass-bgimg-boxpos/form.php
+++ omeka/plugins_lf/ExhibitBuilder/views/shared/exhibit_layouts/ddb-litfass-bgimg-boxpos/form.php
@@ -1,7 +1,9 @@
 <fieldset class="<?php echo html_escape($layout); ?>">
     <div class="section">
         <h4><?php echo __('Hintergrundbild'); ?></h4>
-        <?php echo ExhibitDdbHelper::exhibit_builder_layout_form_item(1, true); ?>
+        <div class="hide-caption">
+            <?php echo ExhibitDdbHelper::exhibit_builder_layout_form_item(1, true); ?>
+        </div>
         <h4><?php echo __('Textblock'); ?></h4>
         <?php echo exhibit_builder_layout_form_text(1); ?>
     </div>
